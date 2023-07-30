d012b1b9a8c32b1d0c7640bce37c8829450bb6a8
Chris Candelora <chris@inigus.com>
2023-07-29 20:44:03 -0400

    changing to port 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const Post = require('./models/Post');
 const bodyParser = require("body-parser");
